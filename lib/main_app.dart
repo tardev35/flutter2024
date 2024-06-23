c2626a14c3b7ccf50edd5d3bd6984f1cf8275d0b
tardev35 <suksanban1992@gmail.com>
2024-06-23 13:37:55 +0700

    Add Icon in side Menu

--- lib/main_app.dart
+++ lib/main_app.dart
@@ -11,20 +11,45 @@ class MainApp extends StatelessWidget {
         home: Scaffold(
           appBar: AppBar(
               title: const Text('My App Flutter'),
-              backgroundColor: Color.fromARGB(255, 6, 208, 77),
+              backgroundColor: const Color.fromARGB(255, 6, 208, 77),
               actions: <Widget>[
-                IconButton(
-                  onPressed: () {
-                    print('Notification icon pressed');
+                PopupMenuButton<String>(
+                  onSelected: (String result) {
+                    print('Selected $result');
                   },
-                  icon: const Icon(Icons.notifications),
-                ),
-                IconButton(
-                  onPressed: () {
-                    print('Setting Icon');
-                  },
-                  icon: const Icon(Icons.search_outlined),
-                ),
+                  itemBuilder: (BuildContext context) => [
+                    const PopupMenuItem(
+                      value: 'Setting',
+                      child: Row(
+                        children: <Widget>[
+                          Icon(Icons.settings),
+                          SizedBox(width: 10),
+                          Text('Settiing'),
+                        ],
+                      ),
+                    ),
+                    const PopupMenuItem(
+                      value: 'Profile',
+                      child: Row(
+                        children: <Widget>[
+                          Icon(Icons.people),
+                          SizedBox(width: 10),
+                          Text('Profile')
+                        ],
+                      ),
+                    ),
+                    const PopupMenuItem(
+                      value: 'Logout',
+                      child: Row(
+                        children: <Widget>[
+                          Icon(Icons.logout),
+                          SizedBox(width: 10),
+                          Text('Logout')
+                        ],
+                      ),
+                    )
+                  ],
+                )
               ]),
           body: const Center(
             child: Text('Wellcome Flutter'),
