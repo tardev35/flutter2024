ef0fcfd06771b8ea9d0a7a4d94919b1f624e4f78
tardev35 <suksanban1992@gmail.com>
2024-06-25 20:33:31 +0700

    UserAccountsDrawerHeader

--- lib/drawer_example.dart
+++ lib/drawer_example.dart
@@ -25,14 +25,27 @@ class DrawerPage extends StatelessWidget {
       drawer: Drawer(
         child: ListView(
           children: const [
-            DrawerHeader(
-              decoration: BoxDecoration(
-                color: Colors.green,
+            // DrawerHeader(
+            //   decoration: BoxDecoration(
+            //     color: Colors.green,
+            //   ),
+            //   child: Text(
+            //     'My Header',
+            //     style: TextStyle(color: Colors.white, fontSize: 25),
+            //   ),
+            // ),
+            UserAccountsDrawerHeader(
+              currentAccountPicture: CircleAvatar(
+                backgroundImage: NetworkImage(
+                    'https://images.unsplash.com/photo-1712847333453-740d9665aa5d?q=80&w=800&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'),
               ),
-              child: Text(
-                'My Header',
-                style: TextStyle(color: Colors.white, fontSize: 25),
+              accountEmail: Text('headerdrawe@gmail.com'),
+              accountName: Text(
+                'Niran Tha',
+                style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.w600),
               ),
+              decoration:
+                  BoxDecoration(color: Color.fromRGBO(1, 181, 70, 0.833)),
             ),
             ListTile(
               leading: Icon(Icons.home),
@@ -49,6 +62,10 @@ class DrawerPage extends StatelessWidget {
             ListTile(
               leading: Icon(Icons.currency_bitcoin),
               title: Text('Payment'),
+            ),
+            ListTile(
+              leading: Icon(Icons.download),
+              title: Text('Order'),
             )
           ],
         ),
