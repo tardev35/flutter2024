10b16248a1cc778f80f44892173b462c5b5bf48d
tardev35 <suksanban1992@gmail.com>
2024-06-27 21:01:39 +0700

    Listview And Card

--- lib/listview_example.dart
+++ lib/listview_example.dart
@@ -30,53 +30,67 @@ class ListviewPage extends StatelessWidget {
       body: ListView(
         padding: const EdgeInsets.all(1),
         children: const <Widget>[
-          ListTile(
-            title: Text('Battery Full'),
-            subtitle: Text('The battery is full.'),
-            leading: Icon(Icons.battery_charging_full),
-            trailing: Icon(Icons.more_vert),
-          ),
-          ListTile(
-            title: Text('Anchor'),
-            subtitle: Text('Lower the anchor.'),
-            leading: Icon(Icons.anchor),
-            trailing: Icon(Icons.more_vert),
-          ),
-          ListTile(
-            title: Text('Access Time'),
-            subtitle: Text('Time To Go'),
-            leading: Icon(Icons.access_time),
-            trailing: Icon(Icons.more_vert),
-          ),
-          ListTile(
-            title: Text('Arrow Forward'),
-            subtitle: Text('Going Out'),
-            leading: Icon(Icons.arrow_forward_sharp),
-            trailing: Icon(Icons.more_vert),
-          ),
-          ListTile(
-            title: Text('Flutter UI'),
-            subtitle: Text('Lern Flutter UI'),
-            leading: CircleAvatar(
-              backgroundImage: AssetImage('assets/fluuter-icon.png'),
+          Card(
+            child: ListTile(
+              title: Text('Battery Full'),
+              subtitle: Text('The battery is full.'),
+              leading: Icon(Icons.battery_charging_full),
+              trailing: Icon(Icons.more_vert),
             ),
-            trailing: Icon(Icons.star),
           ),
-          ListTile(
-            title: Text('Vue.Js Design'),
-            subtitle: Text('Lern Vue3 UI'),
-            leading: CircleAvatar(
-              backgroundImage: AssetImage('assets/vue-icon.png'),
+          Card(
+            child: ListTile(
+              title: Text('Anchor'),
+              subtitle: Text('Lower the anchor.'),
+              leading: Icon(Icons.anchor),
+              trailing: Icon(Icons.more_vert),
             ),
-            trailing: Icon(Icons.star),
           ),
-          ListTile(
-            title: Text('Next.Js Design'),
-            subtitle: Text('Lern Next14 UI'),
-            leading: CircleAvatar(
-              backgroundImage: AssetImage('assets/next-icon.png'),
+          Card(
+            child: ListTile(
+              title: Text('Access Time'),
+              subtitle: Text('Time To Go'),
+              leading: Icon(Icons.access_time),
+              trailing: Icon(Icons.more_vert),
             ),
-            trailing: Icon(Icons.star),
+          ),
+          Card(
+            child: ListTile(
+              title: Text('Arrow Forward'),
+              subtitle: Text('Going Out'),
+              leading: Icon(Icons.arrow_forward_sharp),
+              trailing: Icon(Icons.more_vert),
+            ),
+          ),
+          Card(
+            child: ListTile(
+              title: Text('Flutter UI'),
+              subtitle: Text('Lern Flutter UI'),
+              leading: CircleAvatar(
+                backgroundImage: AssetImage('assets/fluuter-icon.png'),
+              ),
+              trailing: Icon(Icons.star),
+            ),
+          ),
+          Card(
+            child: ListTile(
+              title: Text('Vue.Js Design'),
+              subtitle: Text('Lern Vue3 UI'),
+              leading: CircleAvatar(
+                backgroundImage: AssetImage('assets/vue-icon.png'),
+              ),
+              trailing: Icon(Icons.star),
+            ),
+          ),
+          Card(
+            child: ListTile(
+              title: Text('Next.Js Design'),
+              subtitle: Text('Lern Next14 UI'),
+              leading: CircleAvatar(
+                backgroundImage: AssetImage('assets/next-icon.png'),
+              ),
+              trailing: Icon(Icons.star),
+            ),
           ),
         ],
       ),
